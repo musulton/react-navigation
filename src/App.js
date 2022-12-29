14c8465fc224389ddf5bc431a2d21122780c0d16
Muhammad Sulton <musulton@gmail.com>
2022-12-29 13:22:35 +0700

    Add protected route to some page

--- src/App.js
+++ src/App.js
@@ -1,33 +1,24 @@
 import React from "react";
-import {Routes, Route, Outlet} from "react-router-dom";
+import {Routes, Route, Outlet, Navigate, useNavigate} from "react-router-dom";
 
 import "./App.css";
 import {Navbar} from "./components";
 import {ROUTES} from "./constants";
 import {CourseList, AddType, AddCourse, TypeList, EditCourse, Dashboard} from "./pages";
 
-function Course() {
-    return (
-        <div>
-            <h1>Course</h1>
-            <Outlet />
-        </div>
-    )
+function ProtectedRoute({isLoggedIn}) {
+    if (!isLoggedIn) {
+        return <Navigate to={ROUTES.DASHBOARD} replace />
+    }
+
+    return <Outlet />
 }
 
-
-function CourseType() {
+function Layout({onLogout, onLogin, isLoggedIn}) {
     return (
         <div>
-            <h1>Course Type</h1>
-            <Outlet />
-        </div>
-    )
-}
-
-function Layout() {
-    return (
-        <div>
+            {!isLoggedIn ? (<button onClick={onLogin}>Login</button>) :
+            (<button onClick={onLogout}>Logout</button>)}
             <h1>Enigma Course</h1>
             <Navbar />
             <hr />
@@ -37,17 +28,24 @@ function Layout() {
 }
 
 function App() {
+    const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+    const navigate = useNavigate();
+    const onLogin = () => {
+        setIsLoggedIn(true)
+        navigate(ROUTES.COURSE_LIST)
+    }
+    const onLogout = () => setIsLoggedIn(false);
     return (
         <div className="App">
             <Routes>
-                <Route path={ROUTES.DASHBOARD} element={<Layout />}>
+                <Route path={ROUTES.DASHBOARD} element={<Layout onLogin={onLogin} onLogout={onLogout} isLoggedIn={isLoggedIn} />}>
                     <Route index={true} element={<Dashboard />} />
-                    <Route path={ROUTES.COURSE_LIST} element={<Course />}>
+                    <Route path={ROUTES.COURSE_LIST} element={<ProtectedRoute isLoggedIn={isLoggedIn} />}>
                         <Route index element={<CourseList />} />
                         <Route path={ROUTES.ADD_COURSE} element={<AddCourse />} />
                         <Route path={`${ROUTES.EDIT_COURSE}/:courseId?`} element={<EditCourse />} />
                     </Route>
-                    <Route path={ROUTES.TYPE_LIST} element={<CourseType />}>
+                    <Route path={ROUTES.TYPE_LIST} element={<ProtectedRoute isLoggedIn={isLoggedIn} />}>
                         <Route index element={<TypeList />} />
                         <Route path={ROUTES.ADD_TYPE} element={<AddType />} />
                     </Route>
